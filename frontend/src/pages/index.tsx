df8150d50f76a41c2056c92bb79b550d8778b332
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2020-06-26 03:04:57 +0900

    react playerで再生して、canvasに描画するようにした

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,24 +2,34 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { Layout } from '../components/Layout'
 
+import ReactPlayer from 'react-player'
+
 interface VideoProps {
+  showVideoFlag: boolean;
+  videoUrl: string;
 }
 
 export class IndexPage extends React.Component<VideoProps> {
   private canvas: HTMLCanvasElement | undefined;
   private context: CanvasRenderingContext2D | undefined;
-  private video: HTMLVideoElement | undefined;
+  private reactPlayer: ReactPlayer | undefined;
+  private videoRef: HTMLVideoElement | undefined;
 
   constructor(props: any) {
     super(props)
+    this.state = {
+      showVideoFlag: true,
+      videoUrl: 'https://github.com/intel-iot-devkit/sample-videos/raw/master/car-detection.mp4',
+    }
     this.update = this.update.bind(this)
     this.onCanvasLoaded = this.onCanvasLoaded.bind(this)
-    this.onVideoLoaded = this.onVideoLoaded.bind(this)
+    this.onVideoPlayReady = this.onVideoPlayReady.bind(this)
+    this.onVideoProgress = this.onVideoProgress.bind(this);
   }
 
   update(){
-    if(this.context && this.video){
-      this.context.drawImage(this.video, 0, 0, 200, 400);
+    if(this.context && this.videoRef){
+      this.context.drawImage(this.videoRef, 0, 0, this.context.canvas.width, this.context.canvas.height);
     }
     window.requestAnimationFrame(this.update);
   }
@@ -28,24 +38,28 @@ export class IndexPage extends React.Component<VideoProps> {
     if (!canvasRef) {
       return
     }
-    console.log("canvas")
-    console.log(canvasRef);
     this.canvas = canvasRef;
     this.context = canvasRef.getContext("2d");
   }
 
-  onVideoLoaded(videoRef: HTMLVideoElement) {
-    if (!videoRef) {
+  onVideoPlayReady(reactPlayer: ReactPlayer) {
+    if (!reactPlayer) {
       return
     }
-    console.log("Video")
-    this.video = videoRef;
-    videoRef.pause();
-    const self = this;
-    videoRef.addEventListener('loadeddata', function() {
-      videoRef.play();
-      self.update();
-    });
+    this.reactPlayer = reactPlayer;
+    this.videoRef = this.reactPlayer.getInternalPlayer();
+    this.context.canvas.width = parseInt(this.reactPlayer.props.width, 10)
+    this.context.canvas.height = parseInt(this.reactPlayer.props.height, 10)
+    this.videoRef.play();
+    this.state.showVideoFlag = false;
+    this.update();
+  }
+
+  onVideoProgress(buf: any){
+    if (!buf) {
+      return
+    }
+    console.log(buf)
   }
 
   render():
@@ -63,10 +77,14 @@ export class IndexPage extends React.Component<VideoProps> {
       <h2>Hi people</h2>
       <p>Welcome to your new Gatsby site.</p>
       <p>Now go build something great.</p>
-      <div class="wrapper">
-        <canvas id="canvas" ref={this.onCanvasLoaded} ></canvas>
-        <video id="video" src="https://github.com/intel-iot-devkit/sample-videos/raw/master/car-detection.mp4" playsInline autoPlay controls controlslist="nodownload nofullscreen" ref={this.onVideoLoaded} />
-      </div>
+      <canvas id="canvas" ref={this.onCanvasLoaded} ></canvas>
+      <ReactPlayer
+        url={this.state.videoUrl}
+        playing={true}
+        onReady={this.onVideoPlayReady}
+        onProgress={this.onVideoProgress}
+        style={{ display: this.state.showVideoFlag ? 'none' : '' }}
+      />
       <p>
         <Link to="/another-page/">Go to another page</Link>
       </p>
